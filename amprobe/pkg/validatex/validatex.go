54557849e2f662eb5d73c746f479001a9b197cde
amuluze <314901758@qq.com>
2026-06-26 18:41:41 +0800

    perf(validatex): reuse validator instance as package-level singleton

--- amprobe/pkg/validatex/validatex.go
+++ amprobe/pkg/validatex/validatex.go
@@ -8,9 +8,11 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+// validate is a package-level singleton. validator.Validate is concurrency-safe.
+var validate = validator.New()
+
 // ValidateStruct 用于api validate 请求参数
 func ValidateStruct(data interface{}) error {
-	validate := validator.New()
 	err := validate.Struct(data)
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
